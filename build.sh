694f4d820cc49675c26998f0b0b6dcfcdd1110fa
Oskar Holowaty <coding@holowaty.me>
2014-08-14 00:56:53 +0200

    inital commit

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+PACKAGENAME="raspimon"
+MAINTAINER="Rapsimon Team <raspimon@holowaty.me>"
+VENDOR="Rapsimon Team <raspimon@holowaty.me>"
+VERSION="0.1-1"
+URL="https://github.com/bullshit/raspimon"
+LICENSE="GPLv3"
+DESCRIPTION="Raspimon deamon"
+
+#TODO sed version number in changelog and setup.sh
+
+ROOT=`pwd`
+
+cd ./src
+fpm -f -s dir -t deb -a all \
+	-n "$PACKAGENAME" \
+	-m "$MAINTAINER" \
+	--vendor "$VENDOR" \
+	--category "misc" \
+	--license "$LICENSE" \
+	--url $URL \
+	--depends "curl" \
+	--description "$DESCRIPTION" \
+	--after-install "${ROOT}/debian/postinst" \
+	--before-remove "${ROOT}/debian/prerm" \
+	-v "$VERSION" \
+	-p "${ROOT}/${PACKAGENAME}_${VERSION}_all.deb" \
+	--config-files "/etc/" \
+	--deb-changelog "${ROOT}/debian/changelog" \
+	--deb-compression "xz" \
+	.
